e8d27f4da559b66bf7dec10a5ba6d2406fc44f8b
HoangHiep0901 <HoangHiep0901@gmail.com>
2015-08-21 16:00:52 +0700

    commit

--- student-management/src/main/java/com/hoanghiep/spring/controller/AdminController.java
+++ student-management/src/main/java/com/hoanghiep/spring/controller/AdminController.java
@@ -1,8 +1,17 @@
 package com.hoanghiep.spring.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AdminController {
 
+	@RequestMapping("/login")
+	public ModelAndView login (@RequestParam(required = false) boolean error){
+		ModelAndView mav = new ModelAndView("login");
+		if(error) mav.addObject("error", error);
+		return mav;
+	}
 }
--- student-management/src/main/java/com/hoanghiep/spring/controller/HelloController.java
+++ student-management/src/main/java/com/hoanghiep/spring/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.hoanghiep.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -12,4 +14,5 @@ public class HelloController {
 		model.addAttribute("message", "Hello Java Spring");
 		return "index";
 	}
+	
 }
