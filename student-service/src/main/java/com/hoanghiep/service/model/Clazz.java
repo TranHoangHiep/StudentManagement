9042a32a9aa5698a45af9a7b40bd3d54e2591a0a
HoangHiep <hoanghiep@HoangHieps-Mac-Pro.local>
2016-01-12 22:58:06 +0700

    home

---
+++ student-service/src/main/java/com/hoanghiep/service/model/Clazz.java
@@ -0,0 +1,42 @@
+package com.hoanghiep.service.model;
+
+public class Clazz {
+
+	private int id;
+	private String code;
+	private String className;
+	private String createDate;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getClassName() {
+		return className;
+	}
+
+	public void setClassName(String className) {
+		this.className = className;
+	}
+
+	public String getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(String createDate) {
+		this.createDate = createDate;
+	}
+
+}
