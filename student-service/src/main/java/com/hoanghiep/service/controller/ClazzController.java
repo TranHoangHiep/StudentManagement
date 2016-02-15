dbfd72e932c15fb3f8fc7cdf3777828f6bfb54a7
HoangHiep <hoanghiep@HoangHieps-Mac-Pro.local>
2016-02-15 23:02:13 +0700

    home

--- student-service/src/main/java/com/hoanghiep/service/controller/ClazzController.java
+++ student-service/src/main/java/com/hoanghiep/service/controller/ClazzController.java
@@ -1,8 +1,14 @@
 package com.hoanghiep.service.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.hoanghiep.service.dao.ClazzDAO;
 
 public class ClazzController {
 
+	@Autowired
+	private ClazzDAO classDao;
+	
 //	public String addClazz(Clazz clazz, ModelMap model){
 ////		model.addAttribute("code", )
 //	}
