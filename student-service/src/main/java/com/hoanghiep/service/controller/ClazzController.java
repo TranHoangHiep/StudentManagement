ffb01b37af6f2a568f6f82609b2fbecfadfe6824
HoangHiep <hoanghiep@HoangHieps-Mac-Pro.local>
2016-02-22 23:04:28 +0700

    home

--- student-service/src/main/java/com/hoanghiep/service/controller/ClazzController.java
+++ student-service/src/main/java/com/hoanghiep/service/controller/ClazzController.java
@@ -1,15 +1,31 @@
 package com.hoanghiep.service.controller;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.hoanghiep.service.dao.ClazzDAO;
+import com.hoanghiep.service.model.Clazz;
 
+@Controller(value="class")
 public class ClazzController {
 
 	@Autowired
-	private ClazzDAO classDao;
+	public ClazzDAO clazzDao;
 	
+	@RequestMapping(value="/addClass", method=RequestMethod.GET)
 	public void addClazz(){
-		
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		Calendar calendar= Calendar.getInstance();
+		Clazz clazz = new Clazz();
+		clazz.setCode("KTLT");
+		clazz.setClassName("Ky thuat lap tring");
+		clazz.setCreateDate(dateFormat.format(calendar.getTime()));
+		clazzDao.add(clazz);
 	}
 }
--- student-service/src/main/java/com/hoanghiep/service/dao/impl/ClazzDaoImpl.java
+++ student-service/src/main/java/com/hoanghiep/service/dao/impl/ClazzDaoImpl.java
@@ -14,15 +14,20 @@ public class ClazzDaoImpl implements ClazzDAO{
 	
 	public static final String SQL_GET_LIST = "SELECT * FROM class";
 	
-	private static final String SQL_INSERT = "INSERT INTO class";
+	private static final String SQL_INSERT = "INSERT INTO class(code, className, createDate) values(?, ?, ?)";
 
 	private DataSource dataSource;
 	private JdbcTemplate jdbcTemplate;
 	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+		this.jdbcTemplate = new JdbcTemplate();
+	}
+	
 	@Override
 	public void add(Clazz clazz) {
 		// TODO Auto-generated method stub
-		
+		jdbcTemplate.update(SQL_INSERT, clazz.getCode(), clazz.getClassName(), clazz.getCreateDate());
 	}
 
 	@Override
